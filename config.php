b3856a9f690dc41bf11da88dafcf52b8323611cd
Rahmayani <112634823+Rahmaynii@users.noreply.github.com>
2022-11-28 22:47:08 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+//Tugas Pertemuan 12 Rahmayani
+$server = "localhost";
+$user = "root";
+$password = "";
+$nama_database = "pendaftaran_siswa";
+
+$db = mysqli_connect($server, $user, $password, $nama_database);
+
+if( !$db ){
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
+
+?>
